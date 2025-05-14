4a44d053db121f1936263d90bb12d58132ea45de
upskk <upskk@student.kit.edu>
2025-05-14 19:20:58 +0200

    added solver stats method

--- cfmtoolbox-ilp-encoder/cfmtoolbox_ilp_encoder/__init__.py
+++ cfmtoolbox-ilp-encoder/cfmtoolbox_ilp_encoder/__init__.py
@@ -10,35 +10,50 @@ def encode_to_ilp_multiset(cfm: CFM) -> str:
     return solver.ExportModelAsLpFormat(False)
 
 @app.command()
+def get_ilp_multiset_stats(cfm: CFM):
+    solver = create_ilp_multiset_encoding(cfm)
+    print_solver_stats(solver)
+
+def print_solver_stats(solver):
+    print("\nModel Statistics:")
+    print(f"- Variables: {solver.NumVariables()}")
+    print(f"- Constraints: {solver.NumConstraints()}")
+    print(f"- Integer variables: {sum(v.integer() for v in solver.variables())}")
+
+@app.command()
 def run_ilp_solver_maximize_cardinalities(cfm: CFM):
     solver = create_ilp_multiset_encoding(cfm)
     find_actual_max(solver,cfm.root,1)
 
         #print(print(f"{feature.name} = {solver.Objective().Value():0.1f}"))
-    '''
+
     for variable in solver.variables():
         print(f"{variable.name()} = {variable.solution_value()}")
-     '''
+
     #print(solver.ExportModelAsLpFormat(False))
 
 def find_actual_max(solver, feature: Feature, max_parent_cardinality: int):
 
     solver.Maximize(solver.LookupVariable(create_const_name(feature)))
-    solver.Solve()
+    status = solver.Solve()
     #solver.EnableOutput()
+    if status == 0 or status == 1:
+        print(solver.Objective().Value())
+        if int(solver.Objective().Value()) > 1:
+            actual_max = int(solver.Objective().Value()) / max_parent_cardinality
+            new_max = round(max_parent_cardinality * actual_max, None)
+        else:
+            actual_max = int(solver.Objective().Value())
+            new_max = max_parent_cardinality
 
-    if int(solver.Objective().Value()) > 1:
-        actual_max = int(solver.Objective().Value()) / max_parent_cardinality
-        new_max = round(max_parent_cardinality * actual_max, None)
+        if actual_max < get_max_interval_value(feature.instance_cardinality.intervals):
+            print(feature.name + ": ")
+            print("Given feature instance cardinality: " + str(get_max_interval_value(
+                feature.instance_cardinality.intervals)) + "\n")
+            print("Actual Max Feature Instance Cardinality " + str(round(actual_max, None)) + "\n")
     else:
-        actual_max = int(solver.Objective().Value())
-        new_max = max_parent_cardinality
-    if actual_max < get_max_interval_value(feature.instance_cardinality.intervals):
-        print(feature.name + ": ")
-        print("Given feature instance cardinality: " + str(get_max_interval_value(
-            feature.instance_cardinality.intervals)) + "\n")
-        print("Actual Max Feature Instance Cardinality " + str(round(actual_max, None)) + "\n")
-
+        print(status)
+        new_max = 1
     for child in feature.children:
         find_actual_max(solver, child, new_max)
 
@@ -60,7 +75,7 @@ def run_ilp_solver_with_multisetencoding_gap_detection(cfm: CFM):
     for feature in list_features:
         for interval in feature.instance_cardinality.intervals:
             for cardinality in range(interval.lower, interval.upper + 1):
-                new_solver = create_ilp_multiset_encoding(cfm)
+                new_solver = solver
                 constraint = new_solver.Constraint(cardinality, cardinality)
                 constraint.SetCoefficient(solver.LookupVariable(create_const_name(feature)),
                                   1)
